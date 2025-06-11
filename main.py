a2ab3fdb03a17e31027d46734acf9bceea9ce624
Endervalkin <dlmitch8488@gmail.com>
2025-06-11 17:12:09 -0500

    D: updated stats and main

--- main.py
+++ main.py
@@ -1,17 +1,45 @@
-path_to_file = "/mnt/c/bookbot/books/frankenstein.txt"
-def get_book_text(f):
-	with open(path_to_file) as f:
-	# opens the file frankenstein.txt
-		book_text = f.read()
-	
-	return book_text
+import sys
+if len(sys.argv) != 2:
+	print("Usage: python3 main.py <path_to_book>")
+	sys.exit(1)
 
-from stats import word_count
-# This function counts the number of words in the document defined in path to file.
+from stats import (
+    get_word_count, 
+	get_char_count, 
+	sort_characters
+)
+# word_count counts the number of words in the document defined in path to file.
+# char_count counts the number of characters in the document defined in path to file and returns as a dictionary.
 
 def main():
+	path_to_file = sys.argv[1]
 	book_text = get_book_text(path_to_file)
-	print(word_count(book_text), "words found in the document.")
+	word_count = get_word_count(book_text)
+	character_dict = get_char_count(book_text)
+	sort_characters_list = sort_characters(character_dict)
+	print_report(path_to_file, word_count, sort_characters_list)
+
+	
+def get_book_text(path_to_file):
+	with open(path_to_file) as f:
+	# opens the file frankenstein.txt
+		return f.read()
+	
+
+
+def print_report(path_to_file, word_count, sort_characters_list):
+	# prints the report of the book
+	book_text = get_book_text(path_to_file)
+	print("============ BOOKBOT ============")
+	print("Analyzing book found at books/" + path_to_file)
+	print("----------- Word Count ----------")
+	print(f"Found", word_count, "total words.")
+	print("----------- Character Count ----------")
+	for item in sort_characters_list:
+		if not item["char"].isalpha():
+			continue
+		print(f"{item['char']}: {item['num']}")
+	print("============= END ===============")
+
 
 main()
-
--- stats.py
+++ stats.py
@@ -1,4 +1,35 @@
-def word_count(book_text):
+character_dict = {}
+character_list = []
+
+
+def get_word_count(book_text):
 	# counts the number of words in the text
 	words = book_text.split()
 	return len(words)
+
+def get_char_count(book_text):
+	# counts the number of characters in the text
+    lower_text = book_text.lower()
+    for char in lower_text:
+        if char.isalpha() or char.isspace() or char in ".,!?;:'\"-":
+            # Convert character to lowercase for case-insensitive counting
+            # Only count alphabetic characters and spaces
+            if char in character_dict:
+                character_dict[char] += 1
+            else:
+                character_dict[char] = 1
+    return character_dict
+
+def sort_on(d):
+     return d["num"]
+
+def sort_characters(num_chars_dict):
+    sorted_list = []
+    for ch in num_chars_dict:
+        sorted_list.append({"char": ch, "num": num_chars_dict[ch]})
+    sorted_list.sort(key=sort_on, reverse=True)
+    return sorted_list
+     
+
+     
+    
