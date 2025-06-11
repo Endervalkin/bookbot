1d07c9362696f501f8bfeffc9263fc1621dde0e2
Endervalkin <dlmitch8488@gmail.com>
2025-06-11 14:34:56 -0500

    C: created stats.py. moved code for word count to stats.py and add import fuction to main.py

--- main.py
+++ main.py
@@ -2,12 +2,16 @@ path_to_file = "/mnt/c/bookbot/books/frankenstein.txt"
 def get_book_text(f):
 	with open(path_to_file) as f:
 	# opens the file frankenstein.txt
-		return f.read()
-		
+		book_text = f.read()
+	
+	return book_text
+
+from stats import word_count
+# This function counts the number of words in the document defined in path to file.
 
 def main():
 	book_text = get_book_text(path_to_file)
-	print(book_text)
+	print(word_count(book_text), "words found in the document.")
 
 main()
 
---
+++ stats.py
@@ -0,0 +1,4 @@
+def word_count(book_text):
+	# counts the number of words in the text
+	words = book_text.split()
+	return len(words)
